465f0f3b045e137d7fdc4b0eee6b7f153f6c89fd
Katie <katelyn.hertel19@gmail.com>
2016-04-30 13:48:52 -0400

    Some sort of issue with sign up and in

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -265,4 +265,7 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.reset_password_keys = [ :username ]
+  config.confirmation_keys = [ :username ]
+  config.scoped_views = true
 end
