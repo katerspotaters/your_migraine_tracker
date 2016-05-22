c996cd6ebc4e10aa13254d00ad9c2e2d890198bd
Katie <katelyn.hertel19@gmail.com>
2016-05-22 07:49:11 -0700

    attempted survey specs and some styling things

--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe SurveysController, type: :controller do
+  let(:user) { create(:user) }
+  let(:my_survey) { create(:survey)}
 
+  before do
+    sign_in :user, user
+  end
+
+  describe "DELETE #destroy" do
+    it "deletes the survey" do
+      delete :destroy, {user_id: user.id, migraine_id: migraine.id, id: my_survey.id}
+      count = Survey.where(id: my_survey.id).size
+      expect(count).to eq(0)
+    end
+    it "redirects to index" do
+      delete :destroy, {user_id: user.id, migraine_id: migraine, id: my_survey.id}
+      expect(response).to redirect_to user_path
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new, user_id: user.id, migraine_id: migraine.id
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the new view" do
+      get :new, user_id: user.id, migraine_id: migraine.id
+      expect(response).to render_template :new
+    end
+    it "instaniates @survey" do
+      get :new, user_id: user.id, migraine_id: migraine.id
+      expect(assigns(:survey)).not_to be_nil
+    end
+  end
 end
